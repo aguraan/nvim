5bb4424f1d05911e4fc04df2b4c2371ac31a414c
aguraan <agura.iv@gmail.com>
2025-05-11 12:57:33 +0300

    mason 2 update

--- lua/aguraan/plugins/auto-session.lua
+++ lua/aguraan/plugins/auto-session.lua
@@ -1,5 +1,6 @@
 return {
 	"rmagatti/auto-session",
+  lazy = false,
 	keys = {
 		{ "<leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session for cwd" }, -- restore last workspace session for current directory
 		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session for auto session root dir" }, -- save workspace session for current working directory
--- lua/aguraan/plugins/lsp/lspconfig.lua
+++ lua/aguraan/plugins/lsp/lspconfig.lua
@@ -7,34 +7,10 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
+		local keymap = vim.keymap -- for conciseness
 
-		local mason_lspconfig = require("mason-lspconfig")
-
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				-- "tailwindcss",
-				"lua_ls",
-				-- "graphql",
-				"emmet_ls",
-				-- "prismals",
-				-- "pyright",
-				"gopls",
-				"bashls",
-				"pbls",
-			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true, -- not the same as ensure_installed
-		})
 		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		local keymap = vim.keymap -- for conciseness
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -98,83 +74,53 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		lspconfig["protols"].setup({
-			capabilities = capabilities,
-		})
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
+    vim.lsp.config("clangd", {
+      filetypes = { "c", "cpp", "objc", "objcpp" },
+    })
 
-			["clangd"] = function()
-				-- configure clangd server with plugin
-				lspconfig["clangd"].setup({
-					capabilities = capabilities,
-					filetypes = { "c", "cpp", "objc", "objcpp" },
-				})
-			end,
+    vim.lsp.config("gopls", {
+      cmd = { "gopls" },
+      filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      root_markers = { "go.work", "go.mod", ".git" },
+    })
 
-			-- ["graphql"] = function()
-			-- 	-- configure graphql language server
-			-- 	lspconfig["graphql"].setup({
-			-- 		capabilities = capabilities,
-			-- 		filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-			-- 	})
-			-- end,
+    vim.lsp.config("emmet_ls", {
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+    })
 
-			["gopls"] = function()
-				-- configure golangci-lint language server
-				lspconfig["gopls"].setup({
-					capabilities = capabilities,
-					cmd = { "gopls" },
-					filetypes = { "go", "gomod", "gowork", "gotmpl" },
-					root_dir = lspconfig.util.root_pattern("go.work", " go.mod", ".git"),
-				})
-			end,
+    vim.lsp.config("lua_ls", {
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim", "Snacks" },
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
 
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-				})
-			end,
-
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = { -- custom settings for lua
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim", "Snacks" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-							workspace = {
-								-- make language server aware of runtime files
-								library = {
-									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-									[vim.fn.stdpath("config") .. "/lua"] = true,
-								},
-							},
-						},
-					},
-				})
-			end,
-		})
+    vim.lsp.config("graphql", {
+      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+    })
 	end,
 }

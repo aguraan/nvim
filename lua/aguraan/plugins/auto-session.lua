30a9038310cf19edb8edf119ec79afd4eb560d2d
aguraan <agura.iv@gmail.com>
2025-09-21 22:31:45 +0300

    update config

--- lua/aguraan/plugins/auto-session.lua
+++ lua/aguraan/plugins/auto-session.lua
@@ -2,11 +2,15 @@ return {
 	"rmagatti/auto-session",
   lazy = false,
 	keys = {
-		{ "<leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session for cwd" }, -- restore last workspace session for current directory
-		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session for auto session root dir" }, -- save workspace session for current working directory
+		{ "<leader>wr", "<cmd>AutoSession restore<CR>", desc = "Restore session for cwd" }, -- restore last workspace session for current directory
+		{ "<leader>wf", "<cmd>AutoSession search<CR>", desc = "Search sessions" },
+		{ "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session for auto session root dir" }, -- save workspace session for current working directory
 	},
 	opts = {
 		auto_restore = false,
 		suppressed_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    session_lens = {
+      picker = nil, -- "telescope"|"snacks"|"fzf"|"select"|nil Pickers are detected automatically but you can also manually choose one. Falls back to vim.ui.select
+    },
 	},
 }
